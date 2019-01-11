ee305352204ed3377af6177fc9442a6b6bb7c653
Hemapriya S T <46585387+Hemapriya98@users.noreply.github.com>
2019-01-11 21:07:32 +0530

    Update great.py

--- great.py
+++ great.py
@@ -2,8 +2,8 @@ num1=int(input())
 num2=int(input())
 num3=int(input())
 if (num1>num2):
-   print"num1 is greater"
+   print num1 
 elif (num2>num3):
-   print "num2 is greater"
+   print num2 
 else:
-   print "num3 is greater"
+   print num3 
